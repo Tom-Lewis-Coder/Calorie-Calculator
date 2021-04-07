da56722cfa074a58239d2fdc2921c59a9d2b59d4
Tom-Lewis-Coder <64904837+Tom-Lewis-Coder@users.noreply.github.com>
2021-04-07 20:15:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,25 +18,25 @@ const tdeeCalc = (calcAge, calcHeight, calcWeight, ismGender, activity) =>
 const displayResults = tdeeCalc => {
     document.getElementById("resultsContainer").classList.remove("hidden");
     document.getElementById("dailyCaloriesDisplay").textContent = 
-    'Your daily calorie expenditure based on the information you have given is ' + tdeeCalc + ' calories per day'; 
+    "Your daily calorie expenditure based on the information you have given is " + tdeeCalc + " calories per day"; 
 };
 
-document.getElementById('caloriesForm1').addEventListener('submit', function(event){
+document.getElementById("caloriesForm1").addEventListener("submit", function(event){
     event.preventDefault()
 
     displayResults(
         tdeeCalc(
-            calcAge(parseInt(document.getElementById('userAge').value, 10)),
-            calcHeight(parseInt(document.getElementById('userHeight').value, 10)),
-            calcWeight(parseInt(document.getElementById('userWeight').value, 10)),
-            document.getElementById('mGender').checked,
-            parseFloat(document.getElementById('activity').value)
+            calcAge(parseInt(document.getElementById("userAge").value, 10)),
+            calcHeight(parseInt(document.getElementById("userHeight").value, 10)),
+            calcWeight(parseInt(document.getElementById("userWeight").value, 10)),
+            document.getElementById("mGender").checked,
+            parseFloat(document.getElementById("activity").value)
         )
     )
 });
 
   const validateForm2 = () => {
-    let x = document.forms["form2"]["weightGoal"].value
+    let x = document.forms["form2"]["weightGoal"].value;
     let y = document.forms["form2"]["timeFrame"].value;
     if (x == "" || y =="") {
         alert("Please ensure all fields are completed")
@@ -50,28 +50,27 @@ const dailyCals = (calDiff, inDays) => Math.round(calDiff / inDays);
 const displayResults2 = (dailyCals, tdeeCalc) => {
     document.getElementById("resultsContainer2").classList.remove("hidden");
     document.getElementById("dailyDefecitDisplay").textContent = 
-    'In order to attain your goal within the selected time frame, you should consume ' + (dailyCals + tdeeCalc) + ' calories per day'; 
+    "In order to attain your goal within the selected time frame, you should consume " + (dailyCals + tdeeCalc) + " calories per day"; 
 }
 
-document.getElementById('caloriesForm2').addEventListener('submit', function(event){
+document.getElementById("caloriesForm2").addEventListener("submit", function(event){
     event.preventDefault()
 
     displayResults2(
         dailyCals(
-            calDiff(parseInt(document.getElementById('weightGoal').value, 10), 
-                    parseInt(document.getElementById('userWeight').value, 10)),
-            inDays(parseInt(document.getElementById('timeFrame').value, 10))
+            calDiff(parseInt(document.getElementById("weightGoal").value, 10), 
+                    parseInt(document.getElementById("userWeight").value, 10)),
+            inDays(parseInt(document.getElementById("timeFrame").value, 10))
         ),
         tdeeCalc(
-            calcAge(parseInt(document.getElementById('userAge').value, 10)),
-            calcHeight(parseInt(document.getElementById('userHeight').value, 10)),
-            calcWeight(parseInt(document.getElementById('userWeight').value, 10)),
-            document.getElementById('mGender').checked,
-            parseFloat(document.getElementById('activity').value)
+            calcAge(parseInt(document.getElementById("userAge").value, 10)),
+            calcHeight(parseInt(document.getElementById("userHeight").value, 10)),
+            calcWeight(parseInt(document.getElementById("userWeight").value, 10)),
+            document.getElementById("mGender").checked,
+            parseFloat(document.getElementById("activity").value)
         )
     )
 });
-
 
 
 
